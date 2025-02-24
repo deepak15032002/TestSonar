55e351eb532262f02fb20b65107daaa068832771
Deepak Ramawat <101111300+deepak15032002@users.noreply.github.com>
2025-02-24 21:42:17 +0530

    adding new method

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -12,6 +12,14 @@ public class App {
         }
         return a+b;
     }
+    
+    public int getSub(int a, int b){
+        // Infinte loop
+        for(int i=0; i<100; i--){
+            a=a+b;
+        }
+        return a-b;
+    }
 
 
     public void chooseNumber(int num){
